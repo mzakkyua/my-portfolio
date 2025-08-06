45570d5b446acfbb99bc57656f45d8d257e724de
Cycoell <actvtymhs@gmail.com>
2025-08-07 04:17:50 +0700

    terjadi eror dan diharuskan menghapus import svg di main js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './typing.js';
 import './project-modal.js';
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 
